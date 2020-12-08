b508d6eb11624b62bb37bed298381ae39743de1f
Alexey Grigorev <alexey.grigorev@olx.com>
2020-12-08 23:23:44 +0100

    running chrome in docker

--- previews/render.js
+++ previews/render.js
@@ -1,17 +1,18 @@
 const puppeteer = require('puppeteer');
 const path = require("path");
 
-const args = process.argv;
+// const args = process.argv;
 
-const configFile = args[2];
-const config = require('./' + configFile);
+// const configFile = args[2];
+// const config = require('./' + configFile);
 
-console.log("config:", config);
+// console.log("config:", config);
 
-const { title, subtitle, output } = config;
+// const { title, subtitle, output } = config;
 
-// const title = "CRISP-DM";
-// const subtitle = "Machine Learning Process";
+const title = "CRISP-DM";
+const subtitle = "Machine Learning Process";
+const output = 'test.jpg';
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -19,7 +20,9 @@ const { title, subtitle, output } = config;
       width: 940,
       height: 550,
       isLandscape: true
-    }
+    },
+    executablePath: 'google-chrome-stable',
+    args: ['--no-sandbox'],
   });
 
   const page = await browser.newPage();
