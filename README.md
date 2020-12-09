9b61516766f94682aa096e49dd5671730b972ff7
Alexey Grigorev <alexey.grigorev@olx.com>
2020-12-09 10:41:04 +0100

    preview generator

--- README.md
+++ README.md
@@ -22,3 +22,30 @@ bundle exec jekyll serve
 ```
 
 Open [http://localhost:4000](http://localhost:4000)
+
+
+### Generating a cover image
+
+Build the docker image for the cover generator:
+
+```bash
+cd previews
+docker build -t datatalks-cover-generator .
+cd ..
+```
+
+Now let's generate the image:
+
+* the article: `_posts/2020-12-07-practical-guide-better-code.md`
+* the output file: `images/2020-12-07-practical-guide-better-code/cover.jpg`
+
+```bash
+docker run -it \
+    -v $(pwd)/_posts:/app/_posts \
+    -v $(pwd)/_people:/app/_people \
+    -v $(pwd)/images:/app/images \
+    -u $(id -u ${USER}):$(id -g ${USER}) \
+    datatalks-cover-generator \
+    _posts/2020-12-07-practical-guide-better-code.md \
+    images/2020-12-07-practical-guide-better-code/cover.jpg
+```
--- previews/render.js
+++ previews/render.js
@@ -1,20 +1,16 @@
-const puppeteer = require('puppeteer');
+const fs = require('fs');
 const path = require("path");
 
-// const args = process.argv;
 
-// const configFile = args[2];
-// const config = require('./' + configFile);
+const fm = require('front-matter');
+const puppeteer = require('puppeteer');
 
-// console.log("config:", config);
+const args = process.argv;
+const input = args[2];
+const output = args[3];
 
-// const { title, subtitle, output } = config;
 
-const title = "CRISP-DM";
-const subtitle = "Machine Learning Process";
-const output = 'test.jpg';
-
-(async () => {
+const render = async (config, output) => {
   const browser = await puppeteer.launch({
     defaultViewport: {
       width: 940,
@@ -31,10 +27,32 @@ const output = 'test.jpg';
   await page.goto('file://' + fileName);
   await page.waitForSelector('#title');
 
-  await page.evaluate(({title, subtitle}) => {
+  const title = config.title;
+  const subtitle = config.subtitle;
+  
+  const authors = config.authors.map(author => {
+    const dataPath = `_people/${author}.md`;
+
+    const data = fs.readFileSync(dataPath, 'utf8');
+    const attributes = fm(data).attributes;
+
+    const name = attributes.title;
+
+    const image = 'file://' + path.resolve(`./images/authors/${author}.jpg`);
+
+    return { name, image };
+  });
+
+  console.log('authors', authors);
+
+  await page.evaluate(({title, subtitle, authors}) => {
     document.getElementById('title').textContent = title;
     document.getElementById('subtitle').textContent = subtitle;
-  }, { title, subtitle });
+
+    const author = authors[0];
+    document.getElementById('image').setAttribute('src', author.image);
+    document.getElementById('author-name').textContent = author.name;
+  }, { title, subtitle, authors });
 
   await page.screenshot({ 
     path: output,
@@ -42,4 +60,11 @@ const output = 'test.jpg';
   });
  
   await browser.close();
-})();
+};
+
+const data = fs.readFileSync(input, 'utf8');
+const content = fm(data);
+const config = content.attributes;
+
+
+render(config, output);
