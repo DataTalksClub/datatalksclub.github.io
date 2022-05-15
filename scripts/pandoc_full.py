423a7da99a7c99a0423ff98a88e828f54e75ff0f
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2022-05-15 14:41:27 +0200

    triggering from fork

--- scripts/pandoc_full.py
+++ scripts/pandoc_full.py
@@ -1,43 +1,59 @@
+import os
 import sys
 import subprocess
 from pathlib import Path
 import pandoc_process
 
-input = Path(sys.argv[1])
 
-tmp = Path('tmp')
-tmp.mkdir(parents=True, exist_ok=True)
+REMOVE_DOC = os.getenv('REMOVE_DOC', 'False') == 'True'
 
-output = tmp / 'document.md'
-media = tmp / 'media'
+def main(input):
+    if not input.parts[-1].endswith('.docx'):
+        print(f'{input} is not docx, exiting')
+        return
 
-subprocess.call([
-    'pandoc',
-        '-f', 'docx',
-        '-s', input,
-        '--to', 'gfm',
-        '--markdown-headings=atx',
-        '--wrap=none',
-        '--columns=9999',
-        f'--extract-media={media}',
-        '--standalone',
-        '-o', output
-])
+    tmp = Path('tmp')
+    tmp.mkdir(parents=True, exist_ok=True)
 
-post_id = pandoc_process.load_and_process(output)
+    output = tmp / 'document.md'
+    media = tmp / 'media'
 
-subprocess.call([
-    'mv',
-        media / 'media',
-        f'images/posts/{post_id}'
-])
+    subprocess.call([
+        'pandoc',
+            '-f', 'docx',
+            '-s', input,
+            '--to', 'gfm',
+            '--markdown-headings=atx',
+            '--wrap=none',
+            '--columns=9999',
+            f'--extract-media={media}',
+            '--standalone',
+            '-o', output
+    ])
 
-subprocess.call([
-    'gitbash',
-    'scripts/generate-post-preview.sh',
-    post_id
-])
+    post_id = pandoc_process.load_and_process(output)
 
-subprocess.call([
-    'echo', f'::set-output name=post-id::{post_id}'
-])
+    subprocess.call([
+        'mv',
+            media / 'media',
+            f'images/posts/{post_id}'
+    ])
+
+    subprocess.call([
+        'gitbash',
+        'scripts/generate-post-preview.sh',
+        post_id
+    ])
+
+    subprocess.call([
+        'echo', f'::set-output name=post-id::{post_id}'
+    ])
+
+    if REMOVE_DOC == True:
+        subprocess.call([
+            'git', 'rm', {input}
+        ])
+
+if __name__ == '__main__':
+    input = Path(sys.argv[1])
+    main(input)
